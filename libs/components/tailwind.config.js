834dfee222a471d4cb3c2a0b621a61114d8f5208
Stefan Melmuk <stefan.melmuk@gmail.com>
2023-08-24 21:22:07 +0200

    remove bitwarden_license

--- libs/components/tailwind.config.js
+++ libs/components/tailwind.config.js
@@ -4,7 +4,6 @@ const config = require("./tailwind.config.base");
 config.content = [
   "libs/components/src/**/*.{html,ts,mdx}",
   "apps/web/src/**/*.{html,ts,mdx}",
-  "bitwarden_license/bit-web/src/**/*.{html,ts,mdx}",
   ".storybook/preview.tsx",
 ];
 config.safelist = [
