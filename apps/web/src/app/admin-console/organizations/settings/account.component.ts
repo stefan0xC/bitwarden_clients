d2eae77f171328b6105e2afa19b5e952d3b5681e
Stefan Melmuk <stefan.melmuk@gmail.com>
2024-02-05 21:51:25 +0100

    disable flexible collections
    Co-authored-by: BlackDex <black.dex@gmail.com>

--- apps/web/src/app/admin-console/organizations/settings/account.component.ts
+++ apps/web/src/app/admin-console/organizations/settings/account.component.ts
@@ -194,6 +194,7 @@ export class AccountComponent {
   };
 
   submitCollectionManagement = async () => {
+    return; // flexible collections are not supported by Vaultwarden
     // Early exit if self-hosted
     if (this.selfHosted) {
       return;
