3147a6b4bc9de44b2514d9b602bd8afe7f353c51
Stefan Melmuk <stefan.melmuk@gmail.com>
2024-05-21 22:58:40 +0200

    use only gpl code
    remove all references to non-free bitwarden_license code

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,10 +1,6 @@
 /* eslint-disable no-undef, @typescript-eslint/no-var-requires */
 const config = require("../../libs/components/tailwind.config.base");
 
-config.content = [
-  "./src/**/*.{html,ts}",
-  "../../libs/components/src/**/*.{html,ts}",
-  "../../bitwarden_license/bit-web/src/**/*.{html,ts}",
-];
+config.content = ["./src/**/*.{html,ts}", "../../libs/components/src/**/*.{html,ts}"];
 
 module.exports = config;
